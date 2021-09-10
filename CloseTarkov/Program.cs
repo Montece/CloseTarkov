b658b490a9565516673e6d80435c5e43c32e8fb9
Montece <89153577794@mail.ru>
2021-09-11 00:34:29 +0300

    Initial commit

---
+++ CloseTarkov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace CloseTarkov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Process p in Process.GetProcesses()) if (p.ProcessName == "EscapeFromTarkov") p.Kill();
+        }
+    }
+}
